ac5f2c82adc80bdf158f8f49f9ba4b9f9eb357b6
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2020-03-11 17:03:13 +0100

    Add the navigation for snake movement

--- sketch.js
+++ sketch.js
@@ -11,22 +11,14 @@ function draw(){
     s.show();
 }
 
-function Snake(){
-    this.x = 0;
-    this.y = 0;
-    this.xspeed = 1;
-    this.yspeed = 0;
-
-    this.update = function(){
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
-    }
-
-
-    this.show = function(){
-        //make a rectangle white
-        fill(255);
-        //draw a rectangle
-        rect(this.x, this.y, 10, 10);
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        s.dir(0, -1);
+    } else if(keyCode === DOWN_ARROW){
+        s.dir(0, 1);
+    } else if(keyCode === RIGHT_ARROW){
+        s.dir(1, 0);
+    } else if(keyCode === LEFT_ARROW){
+        s.dir(-1, 0);
     }
 }
